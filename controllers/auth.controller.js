f63f7480d39f18e58ff450b278966784f3cbbfc7
Albert <albert.tardis05@gmail.com>
2022-12-17 16:28:27 +0100

    3.0.0

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import { request, response } from "express";
-import { userExistsByEmail } from "../helpers/userExists.js";
+
 import generateJwt from "../helpers/generateJwt.js";
-import { passwordVerify } from "../helpers/passwordHash.js";
+import { findUserByEmail, createUser } from "../helpers/user/index.js";
+import { passwordHash, passwordVerify } from "../helpers/passwordHash.js";
 
 export const loginUser = async (req = request, res = response) => {
   const { email, password } = req.body;
 
   try {
-    const user = await userExistsByEmail(email);
+    const user = await findUserByEmail(email);
 
     // User exists
     if (!user) {
@@ -41,6 +42,54 @@ export const loginUser = async (req = request, res = response) => {
     console.log(error);
     return res.status(500).json({
       message: "Internal server error while trying to log in",
+    });
+  }
+};
+
+export const googleSignIn = async (req = request, res = response) => {
+  const authUser = req.authUser;
+
+  if (!authUser)
+    return res.status(400).json({
+      message: "Bad request - no authenticated user",
+    });
+
+  const { name, picture, email } = authUser;
+
+  try {
+    let user = await findUserByEmail(email);
+
+    if (!user) {
+      const password = passwordHash("google_no_need_password");
+
+      const userToSave = {
+        name,
+        picture,
+        email,
+        password,
+        google: true,
+      };
+
+      user = await createUser(userToSave);
+    }
+
+    if (!user.state)
+      return res.status(401).json({
+        message:
+          "Not authorized - user not active, contact with an administrator",
+      });
+
+    const token = await generateJwt(user.id);
+
+    res.json({
+      message: "Signed in successfully",
+      token,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "ERROR: Google user could not be authenticated",
     });
   }
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { loginUser } from "../controllers/auth.controller.js";
-import validateFields from "../middlewares/validateFields.js";
+import { loginUser, googleSignIn } from "../controllers/auth.controller.js";
+import { validateFields, validateGoogleToken } from "../middlewares/index.js";
 
 const authRoutes = Router();
 
@@ -12,8 +12,14 @@ const middlewares = {
     check("password", "Password is required.").not().isEmpty(),
     validateFields,
   ],
+  google: [
+    check("id_token", "Token is required.").not().isEmpty(),
+    validateGoogleToken,
+    validateFields,
+  ],
 };
 
 authRoutes.post("/login", middlewares.login, loginUser);
+authRoutes.post("/google", middlewares.google, googleSignIn);
 
 export default authRoutes;
