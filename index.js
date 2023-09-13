4007e4fd3e935db396d4a2b9425f699a960b930b
Albert <albert.tardis05@gmail.com>
2023-09-13 20:23:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ import Server from "./models/server.js";
 dotenv.config();
 
 const server = new Server();
-<<<<<<< Updated upstream
 
 server.setup();
 // Un cambio
-=======
 // LOCOOO
 server.setup();
->>>>>>> Stashed changes
